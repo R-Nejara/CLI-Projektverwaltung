bcd308947a39ed51307476609c9e4c97dee152c9
CodeByAlper <aad@doppelde.de>
2026-05-08 11:29:27 +0200

    feat: allow assigning members to tasks

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,6 +1,8 @@
 package src.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Task {
@@ -8,34 +10,38 @@ public class Task {
     private String title;
     private String description;
     private State state;
+    private List<Member> assignees = new ArrayList<>();
     private LocalDateTime dueDate;
 
-    public Task(UUID id, String title, String description, State state, LocalDateTime dueDate) {
+    public Task(UUID id, String title, String description, State state, List<Member> assignees, LocalDateTime dueDate) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.state = (state == null) ? State.OPEN : state;
+        this.assignees = assignees;
         this.dueDate = dueDate;
     }
 
-    public Task(String title, String description, State state, LocalDateTime dueDate) {
+    public Task(String title, String description, State state, List<Member> assignees, LocalDateTime dueDate) {
         this.id = UUID.randomUUID();
         this.title = title;
         this.description = description;
         this.state = (state == null) ? State.OPEN : state;
+        this.assignees = assignees;
         this.dueDate = dueDate;
     }
 
     // Getter
-    public UUID getId() { return id; }
-    public String getTitle() { return title; }
-    public String getDescription() { return description; }
-    public State getState() { return state; }
-    public LocalDateTime getDueDate() { return dueDate; }
+    public UUID getId() { return this.id; }
+    public String getTitle() { return this.title; }
+    public String getDescription() { return this.description; }
+    public State getState() { return this.state; }
+    public List<Member> getAssignees() { return this.assignees; }
+    public LocalDateTime getDueDate() { return this.dueDate; }
 
     // Setter
     public void setTitle(String newTitle) {
-        if (newTitle != null && title.isBlank()) { return; } //ToDO: Error handling
+        if (newTitle != null && title.isBlank()) { return; } //ToDo: Error handling
         this.title = newTitle;
     }
 
@@ -45,6 +51,15 @@ public class Task {
 
     public void setState(State newState) {
         this.state = (newState == null) ? this.state : state;
+    }
+
+    public void addAssignees(Member... newMembers) {
+        if (newMembers == null) { return ; }
+        this.assignees.addAll(List.of(newMembers));
+    }
+
+    public void removeAssignees(Member... members) {
+        // ToDo
     }
 
     public void setDueDate(LocalDateTime newDueDate) {
