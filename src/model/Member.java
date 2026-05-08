8dfe0d099ea5c9fd4fd7de6ffff0910e9846a41d
CodeByAlper <aad@doppelde.de>
2026-05-08 11:03:45 +0200

    feat: implement basic Member structure

---
+++ src/model/Member.java
@@ -0,0 +1,43 @@
+package src.model;
+
+import java.util.UUID;
+
+public class Member {
+    private final UUID id;
+    private String name;
+    private String role;
+
+    public Member(UUID id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public Member(String name, String role) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.role = role;
+    }
+
+    // Getter
+    public UUID getId() { return this.id; }
+    public String getName() { return this.name; }
+    public String getRole() { return this.role; }
+
+    // Setter
+    public void setName(String newName) {
+        if (newName != null && newName.isBlank()) { return; } //ToDO: Error handling
+        this.name = newName;
+    }
+
+    public void setRole(String newRole) {
+        if (newRole != null && newRole.isBlank()) { return; } //ToDO: Error handling
+        this.role = newRole;
+    }
+
+    // Overrides
+    @Override
+    public String toString() {
+        return ""; //ToDo
+    }
+}
