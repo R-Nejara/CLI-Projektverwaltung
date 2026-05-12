c72dfbc6319663d492b8e56fb40c954659a5395b
CodeByAlper <aad@doppelde.de>
2026-05-12 20:38:52 +0200

    refactor: use View class for all ArgsController outputs

--- src/controller/BaseController.java
+++ src/controller/BaseController.java
@@ -4,32 +4,33 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import src.model.Project;
+import src.view.DefaultView;
+import src.view.View;
 
 public abstract class BaseController implements Controller {
+    private final View view = new DefaultView();
     private final List<Project> projects;
 
     protected  BaseController() {
         //TODO import projects
         this.projects = new ArrayList<>();
+
     }
 
     @Override
     public void addProject(String name, String description, LocalDateTime dueDate) {
-        if (name == null || name.isBlank()) {
-            //TODO
+        Project similaProject = getProjectByName(name);
+        if (similaProject != null) {
+            view.printError(String.format("A project with the name '%s' already exists.", name));
             return;
-        }
-
-        if (getProjectByName(name) != null) {
-            //TODO
+        } else if ((similaProject == null && name == null)) {
             return;
         }
 
         Project newProject = new Project(name, description, dueDate);
         projects.add(newProject);
 
-        //Debug
-        System.out.printf("Project added [Name: %s, Desciption: %s, DueDate: %s]\n", name, description, dueDate);
+        view.printMessage(String.format("Project added [Name: %s, Desciption: %s, DueDate: %s]\n", name, description, dueDate));
     }
 
     @Override
@@ -40,25 +41,26 @@ public abstract class BaseController implements Controller {
                                         .filter(p -> p.getTitle().toLowerCase().contains(searchText))
                                         .toList();
 
-        //TODO
-        //Debug
-        results.forEach(p -> System.out.println("- " + p.getTitle()));
+        if (results == null || results.isEmpty()) {
+            view.printWarning(String.format("No projects found."));
+            return;
+        }
+
+        view.printProjectList(results);
     }
 
     @Override
     public void showProject(String name) {
-        Project result = getProjectByName(name);
+        Project project = getProjectByName(name);
+        if (project == null) { return; } 
 
-        System.out.println("- " + ((result != null) ? result.getTitle() : "null"));
+        view.printProject(project);
     }
 
     @Override
     public void editProject(String name, String newName, String description, LocalDateTime dueDate) {
         Project project = getProjectByName(name);
-        if (project == null) {
-            //TODO
-            return;
-        } 
+        if (project == null) { return; } 
 
         if (newName != null && !newName.isBlank()) {
             project.setName(newName);
@@ -71,30 +73,41 @@ public abstract class BaseController implements Controller {
         if (dueDate != null) {
             project.setDueDate(dueDate);
         }
+
+        //TODO view.printMessage();
+        view.printWarning("This feature is not yet implemented.");
     }
 
     @Override
     public void removeProjects(String[] projectNames) {
-        if (projectNames == null) {
-            //TODO
-            //return;
+        if (projectNames == null || projectNames.length <= 0) {
+            view.printError("Specify at least one project name to remove.");
+            return;
         }
         //TODO
+        view.printWarning("This feature is not yet implemented.");
     }
 
     // private functions
 
     private Project getProjectByName(String name) {
-        String searchText = (name != null) ? name.toLowerCase() : null;
+        String searchText = (name != null && !name.isBlank()) ? name.toLowerCase() : null;
 
         if (searchText == null) {
+            view.printError("Project name cannot be empty or null.");
             return null;
         }
 
-        return this.projects
-                    .stream()
-                    .filter(p -> p.getTitle().equalsIgnoreCase(searchText))
-                    .findFirst()
-                    .orElse(null);
+        Project result = this.projects
+                            .stream()
+                            .filter(p -> p.getTitle().equalsIgnoreCase(searchText))
+                            .findFirst()
+                            .orElse(null);
+
+        if (result == null) {
+            view.printError(String.format("Project '%s' not found.", name));
+        }
+            
+        return result;
     }
 }
