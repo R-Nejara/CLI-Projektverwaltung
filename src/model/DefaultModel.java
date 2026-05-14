f6a75e82f55bbd66eef3bbde4aa94a39733df160
Alper D. <aad@doppelad.de>
2026-05-14 22:48:45 +0200

    Add filePath field and constructor to DefaultModel
    Added filePath variable and constructor to DefaultModel.

--- src/model/DefaultModel.java
+++ src/model/DefaultModel.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DefaultModel implements Model {
+    private String filePath;
+
+    public DefaultModel(String filePath) {
+        this.filePath = filePath;
+    }
+    
     @Override
     public void saveProject(Project project) {
         //TODO
-        System.out.println("Unimplemented method 'exportProject'");
+        System.out.println("Unimplemented method 'saveProject'");
     }
 
     @Override
     public List<Project> loadProjects() {
         //TODO
-        System.out.println("Unimplemented method 'importProjects'");
+        System.out.println("Unimplemented method 'loadProjects'");
         return new ArrayList<>();
     }
 
