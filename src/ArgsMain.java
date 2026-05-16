78deedc3a77f120fca032021b7ba1f92af568553
CodeByAlper <aad@doppelde.de>
2026-05-16 18:25:37 +0200

    refactor(ArgsMain): simplify controller initialization logic

--- src/ArgsMain.java
+++ src/ArgsMain.java
@@ -2,14 +2,11 @@ package src;
 
 import src.controller.ArgsController;
 import src.controller.Controller;
+import src.controller.DefaultController;
 
 public class ArgsMain {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            Controller controller = new ArgsController();
-            controller.run(args);
-        } else {
-            Main.main(args);
-        }
+        Controller controller = (args.length > 0) ? new ArgsController() : new DefaultController();
+        controller.run(args);
     }
 }
