d827647a324ee9e1b02552f625e43c663ba8a4f5
CodeByAlper <aad@doppelde.de>
2026-05-26 12:52:45 +0200

    fix(Main): replace BackupView with DefaultView in main method

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ package src;
 
 import src.controller.Controller;
 import src.controller.DefaultController;
-import src.view.BackupView;
+import src.view.DefaultView;
 import src.view.View;
 
 public class Main {
     public static void main(String[] args) {
-        View view = new BackupView();
+        View view = new DefaultView();
         Controller controller = new DefaultController(view);
         controller.run(args);
     }
