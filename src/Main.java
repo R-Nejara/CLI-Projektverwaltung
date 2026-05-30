265fd521e9e0612c168be60c34f592665871c20a
CodeByAlper <aad@doppelde.de>
2026-05-30 22:50:54 +0200

    refactor(Main): replace DefaultController with BackupController and streamline argument handling

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 package src;
 
+import src.controller.ArgsController;
+import src.controller.BackupController;
 import src.controller.Controller;
-import src.controller.DefaultController;
-import src.view.DefaultView;
+import src.view.BackupView;
 import src.view.View;
 
 public class Main {
     public static void main(String[] args) {
-        View view = new DefaultView();
-        Controller controller = new DefaultController(view);
+        View view = new BackupView();
+        Controller defaultController = new BackupController(view);
+        Controller controller = (args.length > 0) ? new ArgsController(view) : defaultController;
         controller.run(args);
     }
 }
