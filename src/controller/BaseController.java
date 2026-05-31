7d90daf998da64daac9344f6a9affec8b49e5e5d
R-Nejara <rayannejara22u@gmail.com>
2026-05-31 12:45:42 +0200

    fix: wrong method name switch from listMembers to printMembers

--- src/controller/BaseController.java
+++ src/controller/BaseController.java
@@ -399,7 +399,7 @@ public abstract class BaseController implements Controller {
             return;
         }
 
-        view.listMembers(results);
+        view.printMembers(results);
     }
 
     /**
